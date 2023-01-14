ee49d4a23cbc07bff511b3f7802c0cdee70a52c6
gaponov <wmconstructor@gmail.com>
2023-01-14 13:42:49 +0200

    Add FofThirdGroupESVReportService and ExtractfromRegisterOfPayers

--- src/main/java/module8/documentApp/ExtractfromRegisterOfPayers.java
+++ src/main/java/module8/documentApp/ExtractfromRegisterOfPayers.java
@@ -1,2 +1,8 @@
-package module8.documentApp;public class ExtractfromRegisterOfPayers {
+package module8.documentApp;
+
+public class ExtractfromRegisterOfPayers implements ReadOnlyDocumentService{
+    @Override
+    public Document generate() {
+        return new Document("Extract from register of payers ");
+    }
 }
--- src/main/java/module8/documentApp/FofThirdGroupESVReportService.java
+++ src/main/java/module8/documentApp/FofThirdGroupESVReportService.java
@@ -1,2 +1,19 @@
-package module8.documentApp;public class FofThirdGroupESVReportService {
+package module8.documentApp;
+
+public class FofThirdGroupESVReportService extends ReportForFOPDeclarationService{
+
+    @Override
+    public void fill(Document document, DocumentFillingData fillingData) {
+        System.out.println("FofThirdGroupESVReportService fill data");
+        String filledDocumentData = document.getDocumentData() + fillingData.getDataToFill();
+
+        document.setDocumentData(filledDocumentData);
+        System.out.println("FofThirdGroupESVReportService declaration:" + document);
+    }
+
+    @Override
+    public Document generate() {
+        System.out.println("FofThirdGroupESVReportService Generating declaration.......");
+        return new Document("Here is my esv declaration:");
+    }
 }
